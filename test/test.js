e2c86f0e4d02cdd45532642e47bcf5a602adcc79
Marco Godínez <markotom@gmail.com>
2015-02-19 18:28:44 -0600

    The instance should have properties provided by Odoo function

--- test/test.js
+++ test/test.js
@@ -1,3 +1,34 @@
-var assert = require('assert');
+var assert = require('assert'),
+    Odoo   = require('../lib/index');
 
-describe('node-odoo', function () {});
+var config = {
+  host: 'http://odoo.domain.com',
+  port: 80,
+  database: 'test',
+  username: 'admin',
+  password: '$72?392.2fhbd'
+};
+
+var odoo = new Odoo(config);
+
+describe('Odoo', function () {
+
+  it('Odoo should be a function and the client its instance', function () {
+    assert.equal(typeof Odoo, 'function');
+    assert(odoo instanceof Odoo);
+  });
+
+  it('Odoo should set properties to instance', function () {
+    assert.notEqual(odoo.host, undefined);
+    assert.equal(odoo.host, config.host);
+    assert.notEqual(odoo.port, undefined);
+    assert.equal(odoo.port, config.port);
+    assert.notEqual(odoo.database, undefined);
+    assert.equal(odoo.database, config.database);
+    assert.notEqual(odoo.username, undefined);
+    assert.equal(odoo.username, config.username);
+    assert.notEqual(odoo.password, undefined);
+    assert.equal(odoo.password, config.password);
+  });
+
+});
